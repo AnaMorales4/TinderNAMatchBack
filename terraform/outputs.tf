78ee72342844f7c11a838c1d5ca8275d56bd3903
sebasrpg1232 <sebasrpg1232@gmail.com>
2025-11-13 00:35:30 -0500

    add: outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,10 @@ output "websocket_ip" {
   value = aws_eip.ws_ip.public_ip
 }
 
+output "backend_public_ip" {
+  value = aws_eip.backend_ip.public_ip
+}
+
+output "ws_public_ip" {
+  value = aws_eip.ws_ip.public_ip
+}
